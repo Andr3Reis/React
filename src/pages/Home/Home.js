4f968282a4e4842d16a9a26468c237736a6e6a42
Reis <andr3luiz.r3is@gmail.com>
2024-05-29 20:34:04 -0300

    Commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavBar } from "../../components/NavBar";
+import axios from "axios";
 
 export function Home() {
-  return <NavBar />;
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setPokemon(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <h1>Pokemon</h1>
+      <ul>
+        {pokemon.map((poke) => {
+          return <li>{poke.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
